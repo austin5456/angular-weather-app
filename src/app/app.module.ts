78f25f99879a4759b8f5efaeb430aa135eeeae85
austin5456 <austin1612@gmail.com>
2017-10-17 12:44:03 -0500

    added lite-server to devDep, trasnfered the Css and Html to Master-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MasterLayoutComponent } from "./master-layout.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterLayoutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/master-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: "master-layout",
+	styleUrls: ["./master-layout.component.css"],
+	templateUrl: "./master-layout.component.html"
+})
+export class MasterLayoutComponent {
+	title:string = "Master Layout Linked";
+}
