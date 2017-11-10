c747eb7a68e91358d39cdd9f8e882b50834ee126
austin5456 <austin1612@gmail.com>
2017-11-09 22:15:52 -0600

    Location Component is now ready to receive data

--- src/app/location.component.ts
+++ src/app/location.component.ts
@@ -1,8 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { DataService } from "./data.service";
 
+
+
+interface Location {
+	city: string,
+	state: string
+}
 @Component({
 	selector: "location",
 	styleUrls: ["./location.component.css"],
 	templateUrl: "./location.component.html"
 })
-export class LocationComponent {}
+export class LocationComponent implements OnInit{
+	private location: Location;
+
+	constructor(
+		@Inject(DataService) public dataService
+		){}
+
+	render(trimmedData: Location): void {
+		this.location = trimmedData;
+	}
+	handleData(data): void {
+		let locationHolder = this.trimData(data);
+		this.render(locationHolder);
+	}
+
+	trimData(data): Location {
+
+		let location: Location = {
+			city: data.current_observation.display_location.city,
+			state: data.current_observation.display_location.state_name
+		}
+		return location;
+
+	}
+
+	ngOnInit(): void {
+		
+	}
+}
