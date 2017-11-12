da9670ef4ce7b7e40b40e189c78b4103493937d8
austin5456 <austin1612@gmail.com>
2017-11-12 12:19:10 -0600

    master-layout background and sun now have a night setting dependant on hourlyData

--- src/app/master-layout.component.ts
+++ src/app/master-layout.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DataService } from "./data.service";
 
 @Component({
 	selector: "master-layout",
 	styleUrls: ["./master-layout.component.css"],
 	templateUrl: "./master-layout.component.html"
 })
-export class MasterLayoutComponent {
-	title:string = "Master Layout Linked";
+export class MasterLayoutComponent implements OnInit{
+
+	constructor(@Inject(DataService) public dataService){}
+
+
+	private title:string = "Master Layout Linked";
+	private isNight: boolean;
+
+	setDayVsNight(data) {
+		let dayOrNight = (Number(data.hourly_forecast[0].FCTTIME.hour >= 8 && Number(data.hourly_forecast[0].FCTTIME.hour) <= 19) ? "D" : "N");
+		if (dayOrNight === "N") {
+			this.isNight = true;
+		}
+	}
+
+	ngOnInit(){
+		this.dataService.hourlyData.subscribe(data => {
+			this.setDayVsNight(data);
+		});
+	}
 }
