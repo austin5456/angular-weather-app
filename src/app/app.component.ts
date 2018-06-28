6249c87f483d49dacfeb57d9b458bf868e78e91f
Austin Berry <austin1612@gmail.com>
2018-06-28 15:15:04 -0500

    updated files for angular6 with clean CLI + compat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-import { MasterLayoutComponent } from "./master-layout.component";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Weather App';
+  title = 'app';
 }
